4aa98d61e28c55c3bf6c0122895d9bce2e16cdd2
Michaël Blin <mblin@figarocms.fr>
2022-01-26 23:39:52 +0100

    remove unused constants

--- Sources/MB_TextField/Resources/Constants.swift
+++ Sources/MB_TextField/Resources/Constants.swift
@@ -15,25 +15,8 @@ struct UI {
 
 struct Fonts {
 	
-	struct Name {
-		
-		static let Regular:String = "NunitoSans-Regular"
-		static let Bold:String = "NunitoSans-Bold"
-	}
-	
 	struct Size {
 		
 		static let Default:CGFloat = 14.0
-	}
-	
-	struct Content {
-		
-		static let Regular:UIFont = UIFont(name: Fonts.Name.Regular, size: Fonts.Size.Default)!
-		static let Bold:UIFont = UIFont(name: Fonts.Name.Bold, size: Fonts.Size.Default)!
-	}
-	
-	struct Button {
-		
-		static let Navigation:UIFont = UIFont(name: Fonts.Name.Regular, size: Fonts.Size.Default-1)!
 	}
 }
